ad91c2d1b3c6daf51ca46c9326f0e0e454a67c15
Jordan Carney <jordan@jordan-carney.com>
2026-02-28 08:40:57 -0500

    Use curly apostrophe

--- src/content/site.ts
+++ src/content/site.ts
@@ -1,13 +1,13 @@
 export const siteConfig = {
   name: 'Jordan Carney',
-  introText: "Hey, I'm Jordan.",
+  introText: "Hey, I’m Jordan.",
   bio: {
     rolePrefix: 'Currently, Senior Director of UX Engineering at',
     companyName: 'Branch',
     companyUrl: 'https://branchapp.com/',
     previous: 'Previously, I worked at a variety of startups.',
     personal:
-      "Outside of building, I'm big into fitness, investing, and spending time with my family.",
+      "Outside of building, I’m big into fitness, investing, and spending time with my family.",
   },
   email: 'jordan@jordan-carney.com',
   emailSubject: 'Inquiry from jordan-carney.com',
